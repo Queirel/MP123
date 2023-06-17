487c847de640a5e1992762dbee257efb68b754cb
Fede <federicoqueirel@hotmail.com>
2023-06-17 04:43:14 -0300

    postgres added

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,9 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [TypeOrmModule.forFeature([User])]
-
+  imports: [TypeOrmModule.forFeature([User])],
 })
 export class UsersModule {}
